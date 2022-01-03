dff391fce188431882a44ac65a42bd1cb37372e6
garbot <85807072+garbot16@users.noreply.github.com>
2022-01-03 12:02:04 +0700

    Update maintenance.py

--- Music/Plugins/maintenance.py
+++ Music/Plugins/maintenance.py
@@ -5,22 +5,22 @@ from Music.MusicUtilities.database.onoff import (is_on_off, add_on, add_off)
 from Music.MusicUtilities.helpers.filters import command
 
 
-@Client.on_message(command("Musicp") & filters.user(SUDOERS))
+@Client.on_message(command("Musicplayer") & filters.user(SUDOERS))
 async def smex(_, message):
-    usage = "**Usage:**\n/Musicp [enable|disable]"
+    usage = "**Penggunaan:**\n »Try `/musicplayer on`**\n`/musicplayer off`"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     chat_id = message.chat.id
     state = message.text.split(None, 1)[1].strip()
     state = state.lower()
-    if state == "enable":
+    if state == "on":
         user_id = 1
         await add_on(user_id)
-        await message.reply_text("Music Enabled for Maintenance")
-    elif state == "disable":
+        await message.reply_text("Musicplayer berhasil diaktifkan")
+    elif state == "off":
         user_id = 1
         await add_off(user_id)
-        await message.reply_text("Maintenance Mode Disabled")
+        await message.reply_text("Musicplayer berhasil dinonaktifkan")
     else:
         await message.reply_text(usage)
 
